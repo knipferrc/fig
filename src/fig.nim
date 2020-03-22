6622b46c777555f096690a02229dae403553433c
knipferrc <knipferrc@gmail.com>
2020-03-22 16:07:46 -0400

    setup downloader

--- src/fig.nim
+++ src/fig.nim
@@ -1,5 +1,64 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import httpclient, os, parseopt, strutils
+
+type URLArray = array[4, string]
+
+proc writeHelp() =
+  echo """
+  README Fig (download common config files)
+
+  Allowed arguments:
+
+  - h | --help : show help
+  - v | --version : show version
+  - d | --default : dowloads "basic configs for javascript projects"
+    """
+
+proc writeVersion() =
+  echo "README Template Downloader 0.1.0"
+
+proc downloadFile(urls: URLArray) =
+  for url in urls:
+    let splitUrl = url.split('/')
+    let fileName = splitUrl[splitUrl.len() - 1]
+
+    var client = newHttpClient()
+    try:
+      var file = open(fileName, fmWrite)
+      defer: file.close()
+      file.write(client.getContent(url))
+      echo("Success - downloaded template to " & fileName)
+    except IOError as err:
+      echo("Failed to download template: " & err.msg)
+
+proc cli() =
+  var urls: URLArray = [
+    "https://raw.githubusercontent.com/knipferrc/fig/master/configs/.prettierrc.js",
+    "https://raw.githubusercontent.com/knipferrc/fig/master/configs/.editorconfig",
+    "https://raw.githubusercontent.com/knipferrc/fig/master/configs/.eslintrc.js",
+    "https://raw.githubusercontent.com/knipferrc/fig/master/configs/.prettierignore"
+  ]
+
+  if paramCount() == 0:
+    writeHelp()
+    quit(0)
+
+  for kind, key, val in getopt():
+    case kind
+    of cmdLongOption, cmdShortOption:
+      case key
+      of "help", "h":
+        writeHelp()
+        quit()
+      of "version", "v":
+        writeVersion()
+        quit()
+      of "d", "default": discard
+      else:
+        discard
+    else:
+      discard
+
+    downloadFile(urls)
 
 when isMainModule:
-  echo("Hello, World!")
+  cli()
